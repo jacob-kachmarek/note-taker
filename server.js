66d092d03042dbd3d644ce16ccfa14dc3e7935ef
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-07-29 03:01:50 -0700

    adding path for notes page

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
-
 const PORT = 3001;
 
-app.use(express.json());
+// app.use(express.json());
 
 app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
